1d60d95576a5a8f702aeb5dfddd590b5f7733a89
amine rakibe <aminerakibe7@gmail.com>
2023-03-14 19:47:02 +0100

    upload

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: amrakibe <amrakibe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 13:10:28 by amrakibe          #+#    #+#             */
-/*   Updated: 2023/03/14 19:12:28 by amrakibe         ###   ########.fr       */
+/*   Updated: 2023/03/14 19:25:24 by amrakibe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ std::vector<std::string> BitcoinExchange::split(std::string line, char delimiter
     std::string sub;
 
     std::stringstream file(line);
-    while (getline(file, sub, delimiter))
+    while (std::getline(file, sub, delimiter))
     {
         if (sub != "")
             v.push_back(sub);
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amrakibe <amrakibe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 13:10:35 by amrakibe          #+#    #+#             */
-/*   Updated: 2023/03/14 19:12:11 by amrakibe         ###   ########.fr       */
+/*   Updated: 2023/03/14 19:43:19 by amrakibe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,17 @@ int main(int ac, char **av)
 	std::string name = av[1];
 	std::ifstream inp_file(name);
 	std::string out;
-	if (inp_file.is_open())
+	if (!inp_file.is_open())
 	{
+		std::cerr << "Error 1: could not open file." << std::endl;
+	}
+	else
 		while (std::getline(inp_file, out))
 		{
 			std::vector<std::string> vc = ex.split(out, '|');
 			if(vc.size() != 2)
-				std::cerr << "Error: bad input => 2001-42-42" << std::endl;
+				std::cerr << "Error: bad input => 2001-42-42" << std::endl;				
 			std::cout << vc.size() << std::endl;
 		}
-	}
-	else
-		std::cerr << "Error 1: could not open file." << std::endl;
 	return (0);
 }
